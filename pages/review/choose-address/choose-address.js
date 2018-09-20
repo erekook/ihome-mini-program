6d4d74bac365c48f1704af584a23cb834b658d2d
erek <longjietan122@gmail.com>
2018-09-20 18:01:18 +0800

    选择住址页面完成

--- pages/review/choose-address/choose-address.js
+++ pages/review/choose-address/choose-address.js
@@ -5,14 +5,27 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    index: 0,
+    identityList: ['业主', '家属', '长期租客', '限期租客'],
+    apartmentName: '请选择',
+    buildingNo: '',
+    unit: '',
+    roomNo: '',
+    dataList: [],
+    active: 0,
+    chooseTitle: '小区',
+    address: '',
+    identity: '',
+    popupShow: true
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.setData({
+      dataList: ['体验小区1', '体验小区2', '体验小区3']
+    })
   },
 
   /**
@@ -62,5 +75,121 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+
+  /**
+   * 点击选择住址
+   */
+  handleChooseAddress: function () {
+    this.setData({
+      popupShow: false
+    })
+  },
+
+  /**
+   * 选择身份
+   * @param e
+   */
+  bindPickerChange: function(e) {
+    this.setData({
+      index: e.detail.value
+    })
+    this.setData({
+      identity: this.data.identityList[this.data.index]
+    })
+  },
+
+  /**
+   *  重新选择小区
+   */
+  handleApartment: function () {
+    this.setData({
+      apartmentName: '请选择',
+      buildingNo: '',
+      unit: '',
+      roomNo: '',
+      chooseTitle: '小区',
+      active: 0,
+      dataList: ['体验小区1', '体验小区2', '体验小区3']
+    })
+  },
+
+  /**
+   *  重新选择楼栋
+   */
+  handleBuilding: function () {
+    if (this.data.active > 0) {
+      this.setData({
+        buildingNo: '请选择',
+        unit: '',
+        roomNo: '',
+        chooseTitle: '栋',
+        active: 1,
+        dataList: ['11', '22', '35']
+      })
+    }
+  },
+
+  /**
+   *  重新选择单元
+   */
+  handleUnit: function () {
+    if (this.data.active > 1) {
+      this.setData({
+        unit: '请选择',
+        roomNo: '',
+        active: 2,
+        chooseTitle: '单元',
+        dataList: ['1单元', '2单元', '3单元']
+      })
+    }
+  },
+
+  /**
+   * active
+   * 0  小区
+   * 1  楼栋
+   * 2  单元
+   * 3  室
+   * @param item
+   */
+  handleChoose: function (item) {
+    console.log(item)
+    switch (this.data.active) {
+      case 0:
+        this.setData({
+          apartmentName: item.currentTarget.dataset.item,
+          buildingNo: '请选择',
+          chooseTitle: '栋',
+          active: 1,
+          dataList: ['11', '22', '35']
+        })
+        break
+      case 1:
+        this.setData({
+          buildingNo: item.currentTarget.dataset.item + '栋',
+          unit: '请选择',
+          chooseTitle: '单元',
+          active: 2,
+          dataList: ['1单元', '2单元', '3单元']
+        })
+        break
+      case 2:
+        this.setData({
+          unit: item.currentTarget.dataset.item,
+          roomNo: '请选择',
+          chooseTitle: '房间',
+          active: 3,
+          dataList: ['101室', '308室', '409室']
+        })
+        break
+      case 3:
+        this.setData({
+          address: this.data.apartmentName + this.data.buildingNo + this.data.unit + item.currentTarget.dataset.item,
+          popupShow: true
+        })
+        break
+    }
   }
+
 })
